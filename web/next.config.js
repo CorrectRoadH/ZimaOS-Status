041caf64327163bbcb7c1f6bbe2fb6dca96336f7
CorrectRoadH <a778917369@gmail.com>
2024-05-23 08:58:28 +0000

    add base url

--- web/next.config.js
+++ web/next.config.js
@@ -2,6 +2,8 @@
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/modules/zimaos_status', // 将 '/myapp' 替换为你的子路径
+  assetPrefix: '/modules/zimaos_status/',
   distDir: '../raw/usr/share/casaos/www/modules/zimaos_status',
   output: 'export',
 	/* config options here */
